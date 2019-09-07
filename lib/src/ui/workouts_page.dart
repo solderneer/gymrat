9f68d2261e5aa79c97b7912a302bf6775dde3383
Sudharshan <solderneer@gmail.com>
2019-09-07 22:04:49 +0800

    feat: added AppIcons

--- lib/src/ui/workouts_page.dart
+++ lib/src/ui/workouts_page.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import "common_widgets/top_bar.dart";
+import "common_widgets/workout_card.dart";
 
 class WorkoutsPage extends StatefulWidget {
   @override
@@ -9,6 +10,24 @@ class WorkoutsPage extends StatefulWidget {
 }
 
 class _WorkoutsState extends State<WorkoutsPage> {
+  final List<WorkoutCard> cards = <WorkoutCard>[
+    WorkoutCard(
+        title: "Push Split 1",
+        duration: "45m 10s",
+        lastExerciseDate: "Monday, 18 Dec",
+        muscleTags: ["Chest", "Shoulders", "Triceps"]),
+    WorkoutCard(
+        title: "Pull Split 1",
+        duration: "40m 0s",
+        lastExerciseDate: "Tuesday, 19 Dec",
+        muscleTags: ["Back", "Biceps"]),
+    WorkoutCard(
+        title: "Legs",
+        duration: "1h 30min",
+        lastExerciseDate: "Wednesday, 20 Dec",
+        muscleTags: ["Glutes", "Hamstrings", "Calves"])
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +46,14 @@ class _WorkoutsState extends State<WorkoutsPage> {
           onPressed: () => Navigator.pop(context, "/home"),
         ),
       ]),
+      Expanded(
+          child: ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              itemCount: cards.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                    padding: EdgeInsets.all(10.0), child: cards[index]);
+              })),
     ]));
   }
 }
